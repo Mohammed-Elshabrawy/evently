21f8a62c1a1e2b73fe0fa4308526a6f08bcc831a
Mohammed-Elshabrawy <m.e18520029@gmail.com>
2026-02-04 18:22:50 +0200

    online version init

---
+++ lib/models/user_model.dart
@@ -0,0 +1,18 @@
+class MyUser {
+  String id, name, email;
+
+  static const String collectionName = 'user';
+
+  MyUser({this.id = '', required this.name, required this.email});
+
+  MyUser.fromFireStore(Map<String, dynamic> data)
+    : this(id: data['id'], name: data['name'], email: data['email']);
+
+  Map<String, dynamic> toFireStore() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+    };
+  }
+}
